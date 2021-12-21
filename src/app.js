cc3768c4bd4eda9463a49ad21855aef9be537fb3
nehemias10 <nehemiasrivero1@gmail.com>
2021-12-21 02:21:20 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -2,10 +2,53 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//the Dom
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
 
-window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let pronoun = ["The", "My"];
+  let subject = [
+    " boyfriend ",
+    " boss ",
+    " brother ",
+    " girlfriend ",
+    " president ",
+    " cat "
+  ];
+  let possetion = [
+    " a bag ",
+    " the shoes ",
+    " a car ",
+    " a cellphone ",
+    " a notebook "
+  ];
+  let action = [" got  ", " stole ", " broke ", " forgot ", "crash"];
+  let where = [
+    "at the Mall",
+    "in my house",
+    "at the stadium",
+    "at the church",
+    "at my job"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    subject[subjectIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    possetion[possetionIndex] +
+    "" +
+    where[whereIndex]
+  );
 };
